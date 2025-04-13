94e7bc1d98d67706a42cd16ef756d1012234b4f6
Thales <thalesroberto.f@hotmail.com>
2025-04-12 21:50:56 -0300

    chore: Update package.json and emailService.js

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,10 +1,21 @@
-const{ transporter, defautFrom } = require('../config/nodemailer');
-const { parseTemplate } = require('../utils/templateParser');
+// const{ transporter, defautFrom } = require('../config/nodemailer');
+// const { parseTemplate } = require('../utils/templateParser');
+import { transporter, defaultFrom } from '../config/nodemailer.js';
+import {parseTemplate} from '../utils/templateParser.js';
 
 class emailService {
 
     async send() {
-
+    /**
+   * Envia um email usando um template e contexto definidos.
+   * @param {string} to - Endereço de email do destinatário.
+   * @param {string} templateName - Nome do template (pasta em src/templates/).
+   * @param {object} context - Dados para o template (ex: { name: 'Usuário', link: '...' }).
+   * @param {string} [from] - Remetente (opcional, usa defaultFrom se não fornecido).
+   * @param {object} [options] - Opções adicionais do Nodemailer (ex: attachments).
+   * @returns {Promise<object>} - Informações sobre o envio (messageId, etc.) ou lança erro.
+   * @throws {Error} Se houver falha no envio ou no processamento do template.
+   */
         try {
             
         } catch (error) {
