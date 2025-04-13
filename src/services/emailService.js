ba85f1582713b3c3036e82d16afb3f0cab89353e
Thales <thalesroberto.f@hotmail.com>
2025-04-12 21:44:33 -0300

    chore: Implement email sending functionality in emailService.js

---
+++ src/services/emailService.js
@@ -0,0 +1,14 @@
+const{ transporter, defautFrom } = require('../config/nodemailer');
+const { parseTemplate } = require('../utils/templateParser');
+
+class emailService {
+
+    async send() {
+
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
